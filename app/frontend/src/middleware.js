18478d59af7682f3fd76f21e4ed1ab1a98dc5fde
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-09-23 13:07:57 +0700

    [#8] FE: feat(middleware) add app configuration check and cookie management

--- app/frontend/src/middleware.js
+++ app/frontend/src/middleware.js
@@ -4,11 +4,43 @@ import { USER_ROLES } from "./static/config";
 
 const protectedRoutes = ["/profile", "/publications", "/reviews", "/settings"];
 const authRoutes = ["/login"];
+const publicRoutes = ["/", "/about", "/browse", "/compare"];
 
 export default async function middleware(request) {
   const session = request.cookies.get("currentUser")?.value;
   const pathName = request.nextUrl.pathname;
   const response = NextResponse.next();
+
+  // check if app is cofigured
+  const appConfig = request.cookies.get("appConfig")?.value;
+  if (
+    !appConfig &&
+    [...publicRoutes, ...protectedRoutes, ...authRoutes].includes(pathName)
+  ) {
+    /// check if app is configured from the API
+    const reqConfig = await fetch(
+      `${process.env.WEBDOMAIN}/api/v1/setup/?format=json`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "X-Setup-Secret": process.env.SETUP_SECRET_KEY,
+        },
+      },
+    );
+    const configData = await reqConfig.json();
+    if (reqConfig.ok) {
+      console.log("App is configured", configData);
+      response.cookies.set({
+        name: "appConfig",
+        value: "configured",
+        httpOnly: true,
+        expires: new Date(Date.now() + 24 * 60 * 60 * 1000), // 1 day
+      });
+      return response;
+    }
+    return NextResponse.redirect(new URL("/setup", request.url));
+  }
 
   if (!session && protectedRoutes.includes(pathName)) {
     return NextResponse.redirect(new URL("/login", request.url));
