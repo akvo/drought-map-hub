abebf08b9d6c9e02a548148ada569019664b0f6e
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-09-23 12:53:37 +0700

    [#8] backend: Add geojson_file field to SiteConfig model

--- app/backend/api/v1/v1_setup/models.py
+++ app/backend/api/v1/v1_setup/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class SiteConfig(models.Model):
     uuid = models.CharField(max_length=36, unique=True)
     name = models.CharField(max_length=255)
+    geojson_file = models.CharField(max_length=255, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
