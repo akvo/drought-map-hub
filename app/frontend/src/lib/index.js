54ae3629b7eb1ff2675f33bbb7928b5aa6321410
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-09-25 10:25:50 +0700

    [#8] FE: reorganize exports in index.js to include app module and prevent conflicts

--- app/frontend/src/lib/index.js
+++ app/frontend/src/lib/index.js
@@ -1,7 +1,9 @@
-export * from "./api";
 export * from "./storage";
 export * from "./ability";
 export * from "./helper";
+export * from "./app";
 import * as auth from "./auth";
 
 export { auth };
+// Avoid star exports for modules with server actions to prevent conflicts
+export { api, setupApiFormData } from "./api";
